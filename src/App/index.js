4f61f2b91493257e78c1e0b336cd8cea00f8f4b1
Kat Morris <kathryn.leigh.morris@gmail.com>
2019-04-23 21:30:39 -0400

    Imported and rendered basic components in App.

--- src/App/index.js
+++ src/App/index.js
@@ -1,9 +1,18 @@
 import React, { Component } from "react";
 import "./style.css";
+import ChatRooms from "../ChatRooms";
+import Messages from "../Messages";
+import MessageForm from "../MessageForm";
+import NewChatRoom from "../NewChatRoom";
 
 class App extends Component {
   render() {
-    return <div className="App">Hello World</div>;
+    return <div className="App">
+      <ChatRooms />
+      <Messages />
+      <MessageForm />
+      <NewChatRoom />
+    </div>;
   }
 }
 
