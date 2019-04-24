b393e6e02ba5a539e211b64466b583c912b33455
Kat Morris <kathryn.leigh.morris@gmail.com>
2019-04-23 22:12:33 -0400

    Imported Chatkit.

--- src/App/index.js
+++ src/App/index.js
@@ -1,14 +1,19 @@
 import React, { Component } from "react";
 import "./style.css";
+import Chatkit from '@pusher/chatkit';
 import ChatRooms from "../ChatRooms";
 import Messages from "../Messages";
 import MessageForm from "../MessageForm";
 import NewChatRoom from "../NewChatRoom";
 
 class App extends Component {
+
   render() {
     return <div className="App">
+      <ChatRooms />
       <Messages />
+      <MessageForm />
+      <NewChatRoom />
     </div>;
   }
 }
